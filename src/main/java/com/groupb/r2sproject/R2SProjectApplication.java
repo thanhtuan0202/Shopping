6b6977b71cf6aad837a9c00b7e0241c0c360968b
thangnguyen <75868691+thangnguyen2002@users.noreply.github.com>
2023-11-13 17:28:07 +0700

    thang1
    v-1

--- src/main/java/com/groupb/r2sproject/R2SProjectApplication.java
+++ src/main/java/com/groupb/r2sproject/R2SProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class R2SProjectApplication {
-
+	//a
     public static void main(String[] args) {
         SpringApplication.run(R2SProjectApplication.class, args);
     }
